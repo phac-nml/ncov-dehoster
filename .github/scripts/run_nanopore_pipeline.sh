01e3652d15c934f4b106ac4310f9350a1b745bfc
Darian Hole <darian.hole@canada.ca>
2023-02-14 15:37:16 -0600

    add test files to repo

--- .github/scripts/run_nanopore_pipeline.sh
+++ .github/scripts/run_nanopore_pipeline.sh
@@ -3,17 +3,6 @@ set -eo pipefail
 
 # Create a Cache Dir
 mkdir -p conda_cache_dir
-mkdir -p nanopore_fastq
-
-# Get Datasets
-# Covid minimap2 index (not using composite ref due to size, will work without just for the testing)
-wget https://raw.githubusercontent.com/DarianHole/test-datasets/master/minimap2/sars-cov-2_ref.mmi
-# Get partial human genome
-wget https://raw.githubusercontent.com/DarianHole/test-datasets/master/partial_human_ref/partial_hg38_ref.fa.gz
-gunzip partial_hg38_ref.fa.gz
-# Small fastq files
-wget https://raw.githubusercontent.com/DarianHole/test-datasets/master/nanopore_fastq/nanopore-1.fastq.gz -P ./nanopore_fastq
-wget https://raw.githubusercontent.com/DarianHole/test-datasets/master/nanopore_fastq/nanopore-2.fastq.gz -P ./nanopore_fastq
 
 # Workaround for mamba-org/mamba#488
 rm -f /usr/share/miniconda/pkgs/cache/*.json
@@ -24,10 +13,10 @@ nextflow run ./main.nf \
     --cache ./conda_cache_dir \
     --nanopore \
     --minimap2 \
-    --fastq_directory $PWD/nanopore_fastq \
+    --fastq_directory $PWD/.github/data/nanopore/ \
     --run_name 'test-1-minimap2-flat' \
     --flat \
-    --composite_minimap2_index $PWD/sars-cov-2_ref.mmi
+    --composite_minimap2_index $PWD/.github/data/minimap2_index/sars-cov-2_ref.mmi
 
 # Reset and Track
 mv .nextflow.log artifacts/minimap2_flat.nextflow.log
@@ -39,9 +28,9 @@ nextflow run ./main.nf \
     --cache ./conda_cache_dir \
     --nanopore \
     --minimap2 \
-    --fastq_directory $PWD/nanopore_fastq \
+    --fastq_directory $PWD/.github/data/nanopore/ \
     --run_name 'test-2-minimap2-partial-human' \
-    --human_ref $PWD/partial_hg38_ref.fa
+    --human_ref $PWD/.github/data/partial_hg38_ref.fa
 
 # Reset and Track
 mv .nextflow.log artifacts/minimap2_expanded.nextflow.log
